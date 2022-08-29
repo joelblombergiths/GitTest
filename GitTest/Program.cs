0d03cd8c9e8fdcca55c8fb6eb9791ada9634e363
Joel Blomberg <joel.blomberg@iths.se>
2022-08-29 14:45:55 +0200

    ask for name instead

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Joel!");
+Console.Write("Whatäs your name? ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
