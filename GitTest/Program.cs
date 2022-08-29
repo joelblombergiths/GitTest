53e393e72caf389e74e8b16229982d7afcfabe88
Joel Blomberg <joel.blomberg@iths.se>
2022-08-29 14:46:42 +0200

    typo

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Whatäs your name? ");
+Console.Write("What's your name? ");
 string name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}!");
